6a1673c813ca600418774a430b3220ff104b8bbb
Ananya-Singh09 <ananyasingh09nov@gmail.com>
2026-04-21 23:14:24 +0530

    Downgrade from .NET 10.0 to .NET 8.0

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
-app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
 app.UseRouting();
-app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
 app.Run();
